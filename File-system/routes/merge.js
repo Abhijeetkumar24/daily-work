79acad6c36973a02343b2709f8d0b5253a54da3c
Abhijeet <abhijeet.kumar@appinventiv.com>
2023-07-11 00:32:51 +0530

    file-system

---
+++ File-system/routes/merge.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const file1Path = './uploads/txt1.txt';
+  const file2Path = './uploads/txt2.txt';
+  const backupFolderPath = './backup';
+
+  try {
+    const file1Content = fs.readFileSync(file1Path, 'utf8');
+    const file2Content = fs.readFileSync(file2Path, 'utf8');
+    const mergedContent = file1Content + file2Content;
+
+    const timestamp = new Date().getTime();
+    const mergedFileName = `merged_${timestamp}.txt`;
+    const mergedFilePath = `${backupFolderPath}/${mergedFileName}`;
+
+    fs.writeFileSync(mergedFilePath, mergedContent);
+
+    res.send('Files merged successfully.');
+  } catch (error) {
+    res.status(500).send('Error merging files.');
+  }
+});
+
+module.exports = router;
